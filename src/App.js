0edaf33724f2cb02db5147281a2819d064d664b6
farukwebcode21 <farukahmadmk@yahoo.com>
2021-12-25 21:53:53 +0600

    react portfoli project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/Home/About/About';
+import Contact from './components/Home/Contact/Contact';
+import Home from './components/Home/Home/Home';
+import Projects from './components/Home/Projects/Projects';
+import Footer from './components/Shaired/Footer/Footer';
+import Navigation from './components/Shaired/Navigation/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Projects/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/components/Home/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div>
+            This is Project componetsa
+        </div>
+    )
+}
+
+export default Projects
